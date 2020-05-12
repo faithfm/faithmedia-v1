8d188829f45354f569355de924a31cbf56aa35a2
Michael Engelbrecht <michaelndani@gmail.com>
2020-05-12 14:34:04 +1000

    Enable "api_token" auth (for /api/ routes)  (add extra column to "users" table)
    From: https://laravel.com/docs/6.x/api-authentication
    (but didn't add code to automatically add api_token when new user is inserted)
    
    Changes:
    * php artisan make:migration add_api_token_to_users_table --table=users
    * edit migration
    
    To Use:
    * manually add an up-to-80-character random token to this field
    * call an API route, adding the "api_token=XXX" to the GET/POST request parameters

---
+++ database/migrations/2020_04_26_043231_add_api_token_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddApiTokenToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // as per instructions in: https://laravel.com/docs/5.8/api-authentication
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('api_token', 80)->after('email')
+                                ->unique()
+                                ->nullable()
+                                ->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('api_token');
+        });
+    }
+}
