00530e2389c45a629bddad5831b8ca87623c48eb
Lidia Ordonez <lidiaordonez18@gmail.com>
2022-06-16 11:19:49 +1000

    Improve readability - change "selected track's" text color to white instead of black

--- resources/js/components/TheMediaPlaylistPanel.vue
+++ resources/js/components/TheMediaPlaylistPanel.vue
@@ -3,122 +3,83 @@
         <v-card :class="{ playlist }">
             <v-list>
                 <v-list-item-group>
-                    <v-list-item
-                    two-line
-                    v-for="(track, index) in playlist"
-                    :key="track.file"
-                    :class="[
+                    <v-list-item two-line v-for="(track, index) in playlist" :key="track.file" :class="[
                         { selected: track.file === selectedTrackFile },
                         { playing: track.file === indexFile },
                         { bg: track.file !== indexFile },
                         { missingFile: !Boolean(track.bytes) },
-                    ]"
-                    class="my-1"
-                    >
-                        <v-list-item-content
-                            @click="selectTrack(track.file)"
-                            @dblclick="
+                    ]" class="my-1">
+                        <v-list-item-content @click="selectTrack(track.file)" @dblclick="
                             actionsDialogModel = track;
                             selectTrack(track.file);
-                            "
-                        >
+                            ">
                             <v-list-item-title class="mt-n4">
-                                <span class="d-flex flex-nowrap text--secondary" style="word-wrap: break-word; white-space: pre-line" >
-                                        {{ index | numbers }} {{ track.content }} - {{ track.guests }}
-                                </span >
+                                <span class="d-flex flex-nowrap text--secondary"
+                                    style="word-wrap: break-word; white-space: pre-line"
+                                    :style="[track.file === selectedTrackFile ? {'color':'white !important'}:{'color':'black'}]">
+                                    {{ index | numbers }} {{ track.content }} - {{ track.guests }}
+                                </span>
                             </v-list-item-title>
-                            <v-list-item-subtitle
-                            v-show="!showReviews"
-                            class="text--primary text-right text-sm-left"
-                            style="text-oveflow: ellipsis; direction: rtl"
-                            >
+                            <v-list-item-subtitle v-show="!showReviews" class="text--primary text-right text-sm-left"
+                                style="text-oveflow: ellipsis; direction: rtl">
                                 <span class="text--secondary">{{ filePath(track.file) }}</span>
                                 /{{ fileName(track.file) }}
                                 <span style="float: right">
-                                    <v-chip
-                                    v-for="tag in track.tags ? track.tags.split(' ') : []"
-                                    :key="tag"
-                                    x-small
-                                    color="#dde"
-                                    text-color="#444"
-                                    style="
+                                    <v-chip v-for="tag in track.tags ? track.tags.split(' ') : []" :key="tag" x-small
+                                        color="#dde" text-color="#444" style="
                                         margin-left: 2px;
                                         padding-left: 4px;
                                         padding-right: 4px;
-                                    "
-                                    >
-                                    {{ tag }}
+                                    ">
+                                        {{ tag }}
                                     </v-chip>
                                 </span>
                             </v-list-item-subtitle>
                             <v-chip-group v-show="showReviews" class="text--primary">
-                                <v-chip
-                                    small
-                                    class="mb-n1"
-                                    @click="submitSongReview(track.file, '+')"
-                                    :class="{
+                                <v-chip small class="mb-n1" @click="submitSongReview(track.file, '+')" :class="{
                                     myRatings: getRating(track.file, 'myRating') == '+',
-                                    }"
-                                >
+                                    }">
                                     <v-avatar left>
-                                    <v-icon x-small>mdi-thumb-up-outline</v-icon>
+                                        <v-icon x-small>mdi-thumb-up-outline</v-icon>
                                     </v-avatar>
                                     {{ getRating(track.file, "+") }}
                                 </v-chip>
-                                <v-chip
-                                    small
-                                    class="mb-n1"
-                                    @click="submitSongReview(track.file, '-')"
-                                    :class="{
+                                <v-chip small class="mb-n1" @click="submitSongReview(track.file, '-')" :class="{
                                     myRatings: getRating(track.file, 'myRating') == '-',
-                                    }"
-                                >
+                                    }">
                                     <v-avatar left>
-                                    <v-icon x-small>mdi-thumb-down-outline</v-icon>
+                                        <v-icon x-small>mdi-thumb-down-outline</v-icon>
                                     </v-avatar>
                                     {{ getRating(track.file, "-") }}
                                 </v-chip>
-                                <v-chip
-                                    small
-                                    class="mb-n1"
-                                    @click="submitSongReview(track.file, '?')"
-                                    :class="{
+                                <v-chip small class="mb-n1" @click="submitSongReview(track.file, '?')" :class="{
                                     myRatings: getRating(track.file, 'myRating') == '?',
-                                    }"
-                                >
+                                    }">
                                     <v-avatar left>
-                                    <v-icon x-small>mdi-help</v-icon>
+                                        <v-icon x-small>mdi-help</v-icon>
                                     </v-avatar>
                                     {{ getRating(track.file, "?") }}
                                 </v-chip>
                             </v-chip-group>
                             <v-chip-group v-show="showReviews" class="text--primary">
-                                <v-chip
-                                    small
-                                    class="mb-n1"
-                                    @click="
+                                <v-chip small class="mb-n1" @click="
                                     reviewFilename = track.file;
                                     reviewComment =
                                         getRating(reviewFilename, 'myComment') || '';
                                     reviewDialog = true;
-                                    "
-                                    :class="{ myRatings: getRating(track.file, 'myComment') }"
-                                >
+                                    " :class="{ myRatings: getRating(track.file, 'myComment') }">
                                     <v-avatar left>
-                                    <v-icon x-small>mdi-comment-outline</v-icon>
+                                        <v-icon x-small>mdi-comment-outline</v-icon>
                                     </v-avatar>
                                     {{ getRating(track.file, "comments") }}
                                 </v-chip>
                             </v-chip-group>
                         </v-list-item-content>
                         <v-list-item-action>
-                            <v-btn
-                            icon
-                            @click.stop="
+                            <v-btn icon @click.stop="
                                 actionsDialogModel = track;
                                 selectTrack(track.file);
-                            "
-                            >
+                            ">
                                 <v-icon small color="black">mdi-dots-horizontal</v-icon>
                             </v-btn>
                         </v-list-item-action>
@@ -126,58 +87,34 @@
                 </v-list-item-group>
             </v-list>
             <div>
-            <v-card-actions class="d-flex justify-content-center">
-                <v-btn
-                v-show="playlist.length < totalResultsAvailable"
-                dark
-                color="red darken-2"
-                @click="showMoreResults"
-                >Show more Results
-                </v-btn>
-                <v-btn
-                v-show="playlist.length < totalResultsAvailable"
-                dark
-                color="red darken-2"
-                @click="showAllResults"
-                >Show all {{ totalResultsAvailable }} Results
-                </v-btn>
-                <v-btn
-                v-show="showReviews"
-                dark
-                color="red darken-2"
-                @click="summaryDialog = true"
-                >Song Review Summary
-                </v-btn>
-            </v-card-actions>
+                <v-card-actions class="d-flex justify-content-center">
+                    <v-btn v-show="playlist.length < totalResultsAvailable" dark color="red darken-2"
+                        @click="showMoreResults">Show more Results
+                    </v-btn>
+                    <v-btn v-show="playlist.length < totalResultsAvailable" dark color="red darken-2"
+                        @click="showAllResults">Show all {{ totalResultsAvailable }} Results
+                    </v-btn>
+                    <v-btn v-show="showReviews" dark color="red darken-2" @click="summaryDialog = true">Song Review
+                        Summary
+                    </v-btn>
+                </v-card-actions>
             </div>
         </v-card>
-        <ActionsDialog
-            v-model="actionsDialogModel"
-            :formFieldNames="[
+        <ActionsDialog v-model="actionsDialogModel" :formFieldNames="[
             'file',
             'series',
             'numbers',
             'guests',
             'content',
             'tags',
-            ]"
-            :defaultFieldConfig="{ required: true, maxLength: 255 }"
-            :fieldConfig="{ file: { disabled: true }, numbers: { maxLength: 30 } }"
-            @playTrack="playTrack"
-            @updateContentDb="$emit('updateContentDb', $event)"
-        ></ActionsDialog>
-        <SongReviewDialog
-            :reviewDialog.sync="reviewDialog"
-            :reviewComment.sync="reviewComment"
-            :reviewFilename="reviewFilename"
-            :songReviews="songReviews"
-            @submitSongReview="submitSongReview"
-        ></SongReviewDialog>
-        <SongReviewSummaryDialog
-            :summaryDialog.sync="summaryDialog"
-            :songReviews="songReviews"
-            :playlist="playlist"
-        ></SongReviewSummaryDialog>
+            ]" :defaultFieldConfig="{ required: true, maxLength: 255 }"
+            :fieldConfig="{ file: { disabled: true }, numbers: { maxLength: 30 } }" @playTrack="playTrack"
+            @updateContentDb="$emit('updateContentDb', $event)"></ActionsDialog>
+        <SongReviewDialog :reviewDialog.sync="reviewDialog" :reviewComment.sync="reviewComment"
+            :reviewFilename="reviewFilename" :songReviews="songReviews" @submitSongReview="submitSongReview">
+        </SongReviewDialog>
+        <SongReviewSummaryDialog :summaryDialog.sync="summaryDialog" :songReviews="songReviews" :playlist="playlist">
+        </SongReviewSummaryDialog>
     </v-skeleton-loader>
 </template>
 
