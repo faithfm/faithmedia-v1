977c9f3edcf44c695fab9ba34ff78e47a877767b
Lidia Ordonez <lidiaordonez18@gmail.com>
2022-07-19 13:09:56 +1000

    NPM and Composer Update:
    - Updated npm to solved vulnerability issues
    - Led to a number of compatibility fixes:
       - Remove unused style in the Media Playlist Panel and Song Review Dialog
       - npm run prod
    
    - Composer update (mainly to bring update Nova version)

--- resources/js/components/TheMediaPlaylistPanel.vue
+++ resources/js/components/TheMediaPlaylistPanel.vue
@@ -11,12 +11,12 @@
                     ]" class="my-1">
                         <v-list-item-content @click="selectTrack(track.file)" @dblclick="
                             actionsDialogModel = track;
-                            selectTrack(track.file);
-                            ">
+                        selectTrack(track.file);
+                        ">
                             <v-list-item-title class="mt-n4">
                                 <span class="d-flex flex-nowrap text--secondary"
                                     style="word-wrap: break-word; white-space: pre-line"
-                                    :style="[track.file === selectedTrackFile ? {'color':'white !important'}:{'color':'black'}]">
+                                    :style="[track.file === selectedTrackFile ? { 'color': 'white !important' } : { 'color': 'black' }]">
                                     {{ index | numbers }} {{ track.content }} - {{ track.guests }}
                                 </span>
                             </v-list-item-title>
@@ -38,7 +38,7 @@
                             <v-chip-group v-show="showReviews" class="text--primary">
                                 <v-chip small class="mb-n1" @click="submitSongReview(track.file, '+')" :class="{
                                     myRatings: getRating(track.file, 'myRating') == '+',
-                                    }">
+                                }">
                                     <v-avatar left>
                                         <v-icon x-small>mdi-thumb-up-outline</v-icon>
                                     </v-avatar>
@@ -46,7 +46,7 @@
                                 </v-chip>
                                 <v-chip small class="mb-n1" @click="submitSongReview(track.file, '-')" :class="{
                                     myRatings: getRating(track.file, 'myRating') == '-',
-                                    }">
+                                }">
                                     <v-avatar left>
                                         <v-icon x-small>mdi-thumb-down-outline</v-icon>
                                     </v-avatar>
@@ -54,7 +54,7 @@
                                 </v-chip>
                                 <v-chip small class="mb-n1" @click="submitSongReview(track.file, '?')" :class="{
                                     myRatings: getRating(track.file, 'myRating') == '?',
-                                    }">
+                                }">
                                     <v-avatar left>
                                         <v-icon x-small>mdi-help</v-icon>
                                     </v-avatar>
@@ -64,10 +64,10 @@
                             <v-chip-group v-show="showReviews" class="text--primary">
                                 <v-chip small class="mb-n1" @click="
                                     reviewFilename = track.file;
-                                    reviewComment =
-                                        getRating(reviewFilename, 'myComment') || '';
-                                    reviewDialog = true;
-                                    " :class="{ myRatings: getRating(track.file, 'myComment') }">
+                                reviewComment =
+                                    getRating(reviewFilename, 'myComment') || '';
+                                reviewDialog = true;
+                                " :class="{ myRatings: getRating(track.file, 'myComment') }">
                                     <v-avatar left>
                                         <v-icon x-small>mdi-comment-outline</v-icon>
                                     </v-avatar>
@@ -77,8 +77,8 @@
                         </v-list-item-content>
                         <v-list-item-action>
                             <v-btn icon @click.stop="
-                                actionsDialogModel = track;
-                                selectTrack(track.file);
+    actionsDialogModel = track;
+selectTrack(track.file);
                             ">
                                 <v-icon small color="black">mdi-dots-horizontal</v-icon>
                             </v-btn>
@@ -107,7 +107,7 @@
             'guests',
             'content',
             'tags',
-            ]" :defaultFieldConfig="{ required: true, maxLength: 255 }"
+        ]" :defaultFieldConfig="{ required: true, maxLength: 255 }"
             :fieldConfig="{ file: { disabled: true }, numbers: { maxLength: 30 } }" @playTrack="playTrack"
             @updateContentDb="$emit('updateContentDb', $event)"></ActionsDialog>
         <SongReviewDialog :reviewDialog.sync="reviewDialog" :reviewComment.sync="reviewComment"
@@ -125,125 +125,128 @@ import SongReviewSummaryDialog from "./SongReviewSummaryDialog.vue";
 import { laravelUserCan } from "../LaravelUserPermissions";
 
 export default {
-  components: {
-    ActionsDialog,
-    SongReviewDialog,
-    SongReviewSummaryDialog,
-  },
-  data() {
-    return {
-      actionsDialogModel: null,
-      reviewFilename: "",
-      reviewDialog: false,
-      summaryDialog: false,
-      reviewComment: "",
-      show: false,
-    };
-  },
-  props: {
-    playlist: Array,
-    selectedTrackFile: String,
-    indexFile: String,
-    isLoading: Boolean,
-    totalResultsAvailable: Number,
-    songReviews: Array,
-  },
-  computed: {
-    // console: () => console,		// make console.XXX available in templates
-    ratingSummary() {
-      let rs = {};
-      // return rs
-      this.songReviews.forEach((review) => {
-        let summary = rs[review.file] || {};
-        if (review.user_id == LaravelAppGlobals.user.id) {
-          summary["myRating"] = review.rating;
-          summary["myComment"] = review.comments;
-        }
-        if (review.comments)
-          summary["comments"] = summary["comments"]
-            ? summary["comments"] + 1
-            : 1;
-        summary[review.rating] = summary[review.rating]
-          ? summary[review.rating] + 1
-          : 1;
-        rs[review.file] = summary;
-      });
-      return rs;
+    components: {
+        ActionsDialog,
+        SongReviewDialog,
+        SongReviewSummaryDialog,
     },
-    showReviews() {
-      if (this.$route.params.filter == "music-pending") {
-        if (laravelUserCan("review-songs")) return true;
-        else return false;
-      } else return false;
+    data() {
+        return {
+            actionsDialogModel: null,
+            reviewFilename: "",
+            reviewDialog: false,
+            summaryDialog: false,
+            reviewComment: "",
+            show: false,
+        };
     },
-  },
-  filters: {
-    numbers: function (value) {
-      // numbers filter example: 2 -> '02.'
-      let number = value + 1;
-      if (number < 10) {
-        return "0" + number + ".";
-      }
-      return number + ".";
+    props: {
+        playlist: Array,
+        selectedTrackFile: String,
+        indexFile: String,
+        isLoading: Boolean,
+        totalResultsAvailable: Number,
+        songReviews: Array,
     },
-  },
-  methods: {
-    selectTrack(file) {
-      this.$emit("selecttrack", file);
+    computed: {
+        // console: () => console,		// make console.XXX available in templates
+        ratingSummary() {
+            let rs = {};
+            // return rs
+            this.songReviews.forEach((review) => {
+                let summary = rs[review.file] || {};
+                if (review.user_id == LaravelAppGlobals.user.id) {
+                    summary["myRating"] = review.rating;
+                    summary["myComment"] = review.comments;
+                }
+                if (review.comments)
+                    summary["comments"] = summary["comments"]
+                        ? summary["comments"] + 1
+                        : 1;
+                summary[review.rating] = summary[review.rating]
+                    ? summary[review.rating] + 1
+                    : 1;
+                rs[review.file] = summary;
+            });
+            return rs;
+        },
+        showReviews() {
+            if (this.$route.params.filter == "music-pending") {
+                if (laravelUserCan("review-songs")) return true;
+                else return false;
+            } else return false;
+        },
     },
-    playTrack(index) {
-      this.$emit("playtrack", index);
+    filters: {
+        numbers: function (value) {
+            // numbers filter example: 2 -> '02.'
+            let number = value + 1;
+            if (number < 10) {
+                return "0" + number + ".";
+            }
+            return number + ".";
+        },
     },
-    showMoreResults() {
-      this.$emit("showmoreresults");
+    methods: {
+        selectTrack(file) {
+            this.$emit("selecttrack", file);
+        },
+        playTrack(index) {
+            this.$emit("playtrack", index);
+        },
+        showMoreResults() {
+            this.$emit("showmoreresults");
+        },
+        showAllResults() {
+            this.$emit("showallresults");
+        },
+        submitSongReview(file, rating = null, comments = null) {
+            if (rating == this.getRating(file, "myRating")) rating = "";
+            this.$emit("submitSongReview", file, rating, comments);
+        },
+        getRating(file, ratingType) {
+            return this.ratingSummary[file]
+                ? this.ratingSummary[file][ratingType] || 0
+                : 0;
+        },
+        filePath(pathname) {
+            return pathname.split("/").slice(0, -1).join("/");
+        },
+        fileName(pathname) {
+            return pathname.split("/").pop();
+        },
     },
-    showAllResults() {
-      this.$emit("showallresults");
-    },
-    submitSongReview(file, rating = null, comments = null) {
-      if (rating == this.getRating(file, "myRating")) rating = "";
-      this.$emit("submitSongReview", file, rating, comments);
-    },
-    getRating(file, ratingType) {
-      return this.ratingSummary[file]
-        ? this.ratingSummary[file][ratingType] || 0
-        : 0;
-    },
-    filePath(pathname) {
-      return pathname.split("/").slice(0, -1).join("/");
-    },
-    fileName(pathname) {
-      return pathname.split("/").pop();
-    },
-  },
 };
 </script>
 
 <style scoped>
 .selected {
-  color: #000;
-  background-color: rgba(229, 56, 53) !important;
+    color: #000;
+    background-color: rgba(229, 56, 53) !important;
 }
+
 .playing {
-  background-color: rgb(226, 157, 156);
+    background-color: rgb(226, 157, 156);
 }
+
 .bg {
-  background-color: rgba(218, 218, 218, 0.425);
+    background-color: rgba(218, 218, 218, 0.425);
 }
+
 .missingFile {
-  background-color: rgba(144, 144, 144, 0.425);
+    background-color: rgba(144, 144, 144, 0.425);
 }
+
 .playlist {
-  overflow: auto;
+    overflow: auto;
 }
+
 .v-skeleton-loader {
-  padding-bottom: 115px;
+    padding-bottom: 115px;
 }
+
 .myRatings {
-  background-color: #b71c1c !important;
-  color: white;
-}
-.myRatings ::v-deep .v-icon {
-  color: white;
+    background-color: #b71c1c !important;
+    color: white;
 }
 </style>
